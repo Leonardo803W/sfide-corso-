36504592496d7fca50e8bc6eee4eb34c166ff631
Riganelli Leonardo <leoriganelli1@gmail.com>
2024-10-09 18:03:20 +0200

    Update D3.js

--- week-2/week-2-day-3/script/D3.js
+++ week-2/week-2-day-3/script/D3.js
@@ -2,7 +2,7 @@ const starWarsCharacters = [
   {
     name: 'Luke Skywalker',
     height: 172,
-    mass: 277,
+    mass: 77,
     hair_color: 'blond',
     skin_color: 'fair',
     eye_color: 'blue',
@@ -153,7 +153,7 @@ const eyeColor = {
   Ogni personaggio dovrà finire nell'array corrispondente al suo colore degli occhi (al valore della sua proprietà "eye_color").
 */
 
-/*for(let i = 0; i < starWarsCharacters.length; i++)
+for(let i = 0; i < starWarsCharacters.length; i++)
 {
   switch(starWarsCharacters[i].eye_color)
   {
@@ -181,7 +181,7 @@ const eyeColor = {
   console.log("il personaggio di " + starWarsCharacters[i].name + " non ha dgli occhi riconoscibili")
   }
 }
-console.log(eyeColor)*/
+console.log(eyeColor)
 
 /* ESERCIZIO 6
   Usa un while loop per calcolare la massa totale dell'equipaggio. Salvala in una variabile chiamata "crewMass".
@@ -233,6 +233,14 @@ else if(crewMass >= 1000)
 /* ESERCIZIO 8
   Usa un for loop per cambiare il valore della proprietà "gender" di alcuni personaggi dal valore "n/a" a "robot" (Tip: puoi effettuare la riassegnazione del valore corrispondente o creare un nuovo array)
 */
+for(let i = 0; i < starWarsCharacters.length; i++)
+{
+  if(starWarsCharacters[i].gender === 'n/a')
+  {
+    starWarsCharacters[i].gender = 'robot'
+  }
+}
+console.log(starWarsCharacters)
 
 /* --EXTRA-- ESERCIZIO 9
   Utilizzando gli elementi presenti nell'array "femaleCharacters" rimuovi dall'array "charactersNames" le stringhe corrispondenti a personaggi con lo stesso nome.
